f63a2df5ca2a8eb43a9e235ab81a4a833c6b835a
adewumiokunlola2-lgtm <adewumiokunlola2@gmail.com>
2026-08-06 19:40:00 +0100

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,6 @@
+# I am describing my daily routine.
+print("When I wake up, I get ready for the day and walk to school.")
+print("After school, I go home and eat lunch, then sometimes I do my homework, watch TV or play video games.")
+print("In the evening, I have dinner then sometimes take a shower and read before going to bed.")
+print("Sometimes, I might do something different, like go to a restaurant with my family, or to the shop. I like shopping and spending a lot of money.")
+print("I usually  go to bed around 10:00 or 11:00 pm and I get about 7 hours of sleep.")

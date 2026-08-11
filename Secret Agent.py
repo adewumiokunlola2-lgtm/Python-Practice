6aa474ec76aa182dc44e2b0ee7a434b303bd59dd
adewumiokunlola2-lgtm <adewumiokunlola2@gmail.com>
2026-08-11 18:45:52 +0100

    Add files via upload

---
+++ Secret Agent.py
@@ -0,0 +1,21 @@
+name = input("Enter your real name, Agent: ")
+gadget = input("Enter your favorite gadget: ")
+
+agent_number = 7
+speed_rating = 9.5
+mission_count = 12
+height_m = 1.65
+is_active = True
+
+print("Name:", name, type(name))
+print("Gadget:", gadget, type(gadget))
+print("Agent Number:", agent_number, type(agent_number))
+print("Speed Rating:", speed_rating, type(speed_rating))
+print("Mission Count:", mission_count, type(mission_count))
+print("Height (m):", height_m, type(height_m))
+print("Is Active:", is_active, type(is_active))
+
+agent_number_text = str(agent_number)
+mission_count_text = str(mission_count)
+speed_rating_text = str(speed_rating)
+status_text = str(is_active)

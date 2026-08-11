f4fe6c0e1cf7d03514fa1a928d9a1867b196f762
adewumiokunlola2-lgtm <adewumiokunlola2@gmail.com>
2026-08-11 19:06:03 +0100

    Add files via upload

---
+++ Multiple Values.py
@@ -0,0 +1,34 @@
+x = 5
+print(x)
+y = "John"
+print(y)
+codingal = "codingal"
+print(codingal)
+name = input("Enter your name: ")
+print(name)
+
+import keyword
+print(keyword.kwlist)
+
+
+# Creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
+
+
+
+import keyword
+
+# print all the Python keywords
+print("Python keywords are...\n")
+print(keyword.kwlist)
